8ca49ce1d68b5dc7766f2cc3143ffacf5c471a64
mo <mkarimidevops@gmail.com>
2020-04-06 23:33:34 -0500

    attribute added

--- s3.tf
+++ s3.tf
@@ -2,4 +2,12 @@ resource "aws_s3_bucket" "test" {
     bucket = "some-bucket-test-name"
     region = "us-east-1"
     acl = "private"  
+
+    tags = {
+        team = "test-group"
+        env = "prod"
+    }
+    versioning {
+        enabled = true
+    }
 }
