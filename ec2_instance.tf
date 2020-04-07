5ee3a56a0f0d2b7daf6bffc19832ac833a98f991
mo <mkarimidevops@gmail.com>
2020-04-07 00:34:50 -0500

    chaned

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "aws" {
     ami               = "${var.ami}"
-    region            = "us-east-1"
+    region            =  us-east-1
     instance_type     = "t2.micor"
-    security_group    = "${var.sg_group}"
+    security_groups    = "[${var.sg_group}"]
     key_name          = "${var.key_name}"
 }
