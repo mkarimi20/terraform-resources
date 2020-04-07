7bae1c79d9491f37d98b1e8625ba7acd4bec95ea
mo <mkarimidevops@gmail.com>
2020-04-06 23:57:32 -0500

    sg corrected

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "aws" {
     ami               = "ami-0ff760d16d9497662"
     region            = "us-east-1"
     instance_type     = "t2.micor"
-    security_groups   = "default"
+    security_group    = "default"
     key_name          = "mylaptop"
 }
