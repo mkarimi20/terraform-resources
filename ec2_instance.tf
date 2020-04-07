6f17e25650fd7dcb8da5e6e236fb0c2e3e8a7dd1
mo <mkarimidevops@gmail.com>
2020-04-07 00:59:07 -0500

    sec group updated

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "aws" {
     ami               = "${var.ami}"
     region            =  "${var.region}"
     instance_type     = "t2.micor"
-    security_groups    = "[${var.sg_group}"]
+    security_groups    = ["${var.sg_group}"]
     key_name          = "${file"("~/.ssh/id_rsa.pub")}"
 }
